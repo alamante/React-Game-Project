180a06afac302387f8152eeafa1b7a08d6573beb
chris kennedy <chris-kennedy09@hotmail.com>
2023-03-24 01:45:03 +0000

    edited the header-nav.js to add better functionality and added hover states to the buttons. modal now contains functions we will use later to clear highscores

--- src/components/header-nav.js
+++ src/components/header-nav.js
@@ -1,9 +1,16 @@
 import { ReactComponent as Logo } from "../Design-Materials/icons-etc/SVG/Logo.svg";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Modal from 'react-bootstrap/Modal';
 
 function Header() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModal = () => setShowModal(!showModal);
+  const handleClear = () => {
+    {/* we will add the rules here to clear the highscores */}
+  };
+
   return (
     <div>
       <Navbar className="custom-nav">
@@ -15,12 +22,20 @@ function Header() {
             alt="React Bootstrap logo"
           />
         </Navbar.Brand>
-        <Nav.Item className="ml-auto">
-          <Nav.Link className="highscores" eventKey="link">
-            HIGHSCORES
-          </Nav.Link>
-        </Nav.Item>
+        <button className="highscores" onClick={handleModal}>HIGHSCORES</button>
       </Navbar>
+      <Modal show={showModal} backdrop='static' onHide={handleModal}>
+        <Modal.Header className="custom-modal-header" closeButton>
+          <Modal.Title className="highscores-title ">HIGHSCORES</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="custom-modal-body">
+          {/* Add our highscores content here */}
+        </Modal.Body>
+        <Modal.Footer className="custom-modal-footer">
+          <button className="modal-btn" onClick={handleClear}>CLEAR</button>
+          <button className="modal-btn" onClick={handleModal}>CLOSE</button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
