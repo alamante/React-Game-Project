756da2145ca35483f708ed9b9862db9fbe8387e4
chris kennedy <chris-kennedy09@hotmail.com>
2023-03-27 21:16:38 +0100

    updated app js and highscres

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header-nav';
+import SimpleContainer from './components/welcome';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SimpleContainer/>
     </div>
   );
 }
