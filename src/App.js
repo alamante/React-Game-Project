356bb399e9e8bef2c39b287c3e1595e27c4a1d66
KELECHI IGBOKWE <alamante82@yahoo.com>
2023-03-29 00:14:00 +0100

    File modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import './App.css';
 import SimpleContainer from './components/welcome';
 import Header from './components/header-nav';
+import Hangman from "./components/Hangman"
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <SimpleContainer></SimpleContainer>
+     <Hangman />
+     
     </div>
   );
 }
