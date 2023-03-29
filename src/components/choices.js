6236ee8b247a938d641e9a89fc17638e2432591e
chris kennedy <chris-kennedy09@hotmail.com>
2023-03-30 00:15:11 +0100

    edited buton styles as they were missing classes

--- src/components/choices.js
+++ src/components/choices.js
@@ -9,24 +9,28 @@ export default function Choice({ handleGameClick, setShowGame }) {
   setShowGame(true);
  };
  return (
-  <div>
-   <h1 className='header'>CHOOSE YOUR GAME</h1>
-   <div className="choices">
-    <button className="custom-btn" choice-btn disabled={hasMadeSelection}>
-     MAGIC 8 BALL
-    </button>
-    <button
-     className="custom-btn choice-btn"
-     onClick={handleChoiceClick}
-     disabled={hasMadeSelection}
-    >
-     ROCK PAPER SCISSORS
-    </button>
-    <button className="custom-btn choice-btn" disabled={hasMadeSelection}>
-     HANGMAN
-    </button>
+   <div>
+     <h1 className="header">CHOOSE YOUR GAME</h1>
+     <div className="choices">
+       <button
+         className="custom-btn choice-btn"
+         choice-btn
+         disabled={hasMadeSelection}
+       >
+         MAGIC 8 BALL
+       </button>
+       <button
+         className="custom-btn choice-btn"
+         onClick={handleChoiceClick}
+         disabled={hasMadeSelection}
+       >
+         ROCK PAPER SCISSORS
+       </button>
+       <button className="custom-btn choice-btn" disabled={hasMadeSelection}>
+         HANGMAN
+       </button>
+     </div>
    </div>
-  </div>
  );
 }
 
