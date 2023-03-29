cd1ed397542d43ccb9eb85aa81a7bbc6c2d6e990
Chantal Guiheen <chantal@chantals-air.lan>
2023-03-29 18:05:24 +0100

    adding Magic-8-Ball file

---
+++ src/pages/RPS.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './components/header-nav';
+
+function RPS() {
+  return (
+      <h1 className="header"> ROCk PAPER SCISSORS</h1>
+    
+  );
+}
+
+export default RPS;
