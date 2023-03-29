e20918d675599638434a3eaf0d0b0685f0ec7ecd
KELECHI IGBOKWE <alamante82@yahoo.com>
2023-03-29 21:18:34 +0100

    files updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import SimpleContainer from './components/welcome';
 import Header from './components/header-nav';
+import SimpleContainer from './components/welcome';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+<<<<<<< HEAD
       <SimpleContainer></SimpleContainer>
+=======
+      <SimpleContainer/>
+>>>>>>> e3f492cf808416def6ddde000e6d70b1d305fa23
     </div>
   );
 }
