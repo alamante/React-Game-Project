ac7e5256f9916be04dc875a553db378b1cd523a5
KELECHI IGBOKWE <alamante82@yahoo.com>
2023-03-29 19:51:46 +0100

    time component added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import SimpleContainer from './components/welcome';
 import Header from './components/header-nav';
 import Hangman from "./components/Hangman"
+import Time from './components/Time';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Time />
      <Hangman />
      
     </div>
