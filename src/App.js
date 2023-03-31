3867853e6483adc39be5e93fc404909fab8c455a
KELECHI IGBOKWE <alamante82@yahoo.com>
2023-03-31 19:09:56 +0100

    Time added to Homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header-nav";
 import SimpleContainer from "./components/welcome";
+import Time from "./components/Time"
 
 
 function App() {
  return (
   <div className='App'>
    <Header/>
+   <Time />
    <SimpleContainer/>
   </div>
  );
